bab53f67861e34e8061f86af47049abc04c5d9ac
Luis flores <luis.r.f.e@hotmail.com>
2023-12-07 15:27:06 -0600

    crud al 80% faltan validaciones para no duplicar

--- assets/js/admin/formProduct.js
+++ assets/js/admin/formProduct.js
@@ -3,9 +3,16 @@ const product = document.querySelector('#input-product');
 const description = document.querySelector('#input-description');
 const image = document.querySelector('#input-image');
 const stock = document.querySelector('#input-stock');
+const inputGetProduct = document.querySelector('#btn-gets-product');
+const btnDelete = document.querySelector('#btn-delete-product');
 const form = document.querySelector('#form');
 const mensajeDiv = document.querySelector('.mensaje-alerta');
+const alertaForm = document.querySelector('.alerta-find');
+const inputBuscar = document.querySelector('#input-buscar')
+
 form.addEventListener('submit', validarform);
+btnDelete.addEventListener('click', eliminarProduct);
+inputGetProduct.addEventListener('click', obtenerProducto);
 
 
 //***********************CLASE*************************************//
@@ -14,36 +21,104 @@ class ProductsController {
         this.productsList = [];
     }
 
-    addItem(name, description, image,  stock) {
+    addItem(name, description, image, stock) {
         const product = {
 
             "id": new Date().getTime(),
             "name": name,
-            "description": description,
-            "image": image,
-            "stock": stock
+            "descriptions": description,
+            "images": image,
+            "stocks": stock
         };
 
-        
+
         if (localStorage.getItem("products")) {
-            const nuevoObj =JSON.parse(localStorage.getItem("products"));
+            const nuevoObj = JSON.parse(localStorage.getItem("products"));
             this.productsList = [...nuevoObj];
             this.productsList.push(product);
             localStorage.setItem("products", JSON.stringify(this.productsList));
             console.log(this.productsList);
-        }else{
+        } else {
             this.productsList.push(product);
             localStorage.setItem("products", JSON.stringify(this.productsList));
         }
-        
+
     }
+
+
+    deleteProduct(id) {
+        limpiarHtml2();
+        const nuevoObj = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
+        const verificarId = nuevoObj.some(elemento => elemento.id === id);
+        const alertaElement = document.createElement('P');
+
+        if (verificarId) {
+            const eliminarObj = nuevoObj.filter(product => product.id != id);
+            localStorage.setItem("products", JSON.stringify(eliminarObj));
+            alertaElement.classList.add('bg-success', 'text-white');
+            alertaElement.textContent = 'Producto borrado correctamente';
+        } else {
+            alertaElement.classList.add('bg-danger', 'text-white');
+            alertaElement.textContent = 'No existe el producto';
+        }
+        alertaForm.appendChild(alertaElement);
+
+        setTimeout(() => {
+            alertaElement.remove();
+        }, 3000);
+
+    }
+
+
+    getProduct(ID) {
+        const nuevoObj = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
+
+        const { id, name, descriptions, images, stocks } = nuevoObj.filter(product => product.id == ID)[0];
+
+        product.value = name;
+        description.value = descriptions;
+        image.value = images;
+        stock.value = stocks;
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+//***************************Funciones para el producto***************************************************** */
+
+const productsController = new ProductsController();
+function addProduct({ name, description, image, stock }) {
+
+    productsController.addItem(name, description, image, stock);
+}
+
+function eliminarProduct() {
+    const id = parseInt(inputBuscar.value);
+    productsController.deleteProduct(id);
+}
+
+
+function obtenerProducto() {
+    const id = parseInt(inputBuscar.value);
+    productsController.getProduct(id)
 }
 
 
 
 
-
 //*****************************FUNCIONES DE VALIDACION**************************************************** 
+
+
+
+
 function validarform(e) {
     e.preventDefault();
     const productObj = {
@@ -116,19 +191,22 @@ function limpiarHtml() {
 
 }
 
+function limpiarHtml2() {
 
+    while (alertaForm.firstChild) {
+        alertaForm.removeChild(alertaForm.firstChild)
+    }
 
-
-
-
-
-//***************************Funciones para el producto***************************************************** */
-
-const productsController = new ProductsController();
-function addProduct({name, description, image, stock}) {
-  
-    productsController.addItem(name, description, image,  stock);
 }
+
+
+
+
+
+
+
+
+
 
 
 
