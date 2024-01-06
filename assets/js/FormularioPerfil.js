a739bd71dfb6cb86abdafbddd599d4fbed2b5245
Liliana Orozco <lilianaorozco.developer@gmail.com>
2024-01-05 20:21:42 -0600

    Formulario de perfil

---
+++ assets/js/FormularioPerfil.js
@@ -0,0 +1,28 @@
+function cargarImagen() {
+    var input = document.getElementById('imagenInput');
+    var imagenMostrada = document.getElementById('imagenMostrada');
+    
+    var file = input.files[0]; 
+    
+    if (file) {
+        var reader = new FileReader(); 
+        reader.onload = function(e) {
+            imagenMostrada.src = e.target.result; 
+        }
+        reader.readAsDataURL(file); 
+    } 
+}
+
+function onFileSelected(event) {
+    var selectedFile = event.target.files[0];
+    var reader = new FileReader();
+  
+    var imgtag = document.getElementById("imagenMostrada");
+    imgtag.title = selectedFile.name;
+  
+    reader.onload = function(event) {
+      imgtag.src = event.target.result;
+    };
+  
+    reader.readAsDataURL(selectedFile);
+  }
