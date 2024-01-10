afca5ef07deb4340ffd9859bc60ae5775ea62eee
Fernanda Maro <148138579+FernandaMaro@users.noreply.github.com>
2024-01-10 00:23:22 -0600

    + centre formulario, cerre etiquetas y arregle el doble form

---
+++ assets/js/nota.js
@@ -0,0 +1,6 @@
+function verNota() {
+    const nota = document.getElementById("nota");
+    const form = document.getElementById("form");
+    form.style.display = "none";
+    nota.style.display = "block";
+}
